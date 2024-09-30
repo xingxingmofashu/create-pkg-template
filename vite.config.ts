a7010133951544a5bf07717d117e8dd4347d5dc5
xingxingmofashu <xingxingmofashu@outlook.com>
2024-09-30 09:55:09 +0800

    refactor: packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,39 @@
-import dayjs from "dayjs"
+import path from "node:path"
+import alias from "@rollup/plugin-alias"
+import resolve from "@rollup/plugin-node-resolve"
 import { defineConfig } from "vite"
 
-export default defineConfig(({ mode }) => {
-  return {
-    define: {
-      __BUILD_TIME__: JSON.stringify(dayjs().format("YYYY/MM/DD HH:mm")),
+export default defineConfig({
+  plugins: [],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, "packages/index.ts"),
+      name: "create-pkg",
     },
-  }
+    rollupOptions: {
+      output: [
+        {
+          format: "cjs",
+          entryFileNames: "[name].js",
+          exports: "named",
+        },
+        {
+          format: "esm",
+          entryFileNames: "[name].mjs",
+        },
+      ],
+      plugins: [
+        resolve(),
+        alias({
+          entries: [
+            {
+              find: "@create-pkg",
+              replacement: path.resolve(__dirname, "packages"),
+            },
+          ],
+        }),
+      ],
+    },
+  },
 })
